e6b340a4f39a2d32ebe165d8757dc2329f117f96
Matteo Tinta <matteo.tinta.external@ynap.com>
2025-09-24 13:01:05 +0200

    test: added tests for mongo & aws expectations

--- Momo/Expectations/MomoMongoExpectation.cs
+++ Momo/Expectations/MomoMongoExpectation.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using Momo.Steps;
+using MongoDB.Driver;
 
 namespace Momo.Expectations;
 
@@ -7,9 +8,19 @@ public class MomoMongoExpectation: IMomoExpectation
 {
     public string ConnectionString { get; set; }
     public List<MomoMongoQueryExpectation> Match { get; set; }
-    
-    public IStepHandler GetStepHandler(MomoClientFactoryOptions options) 
-        => new MongoStepHandler();
+
+    public IStepHandler GetStepHandler(MomoClientFactoryOptions options)
+    {
+        try
+        {
+            _ = new MongoUrl(ConnectionString);
+            return new MongoStepHandler();
+        }
+        catch (Exception e)
+        {
+            throw new InvalidOperationException("Not a mongo connection string", e);
+        }
+    }
 }
 
 public record MomoMongoQueryExpectation
---
+++ tests/Momo.Tests/Expectations/MomoMongoExpectationTests.cs
@@ -0,0 +1,54 @@
+﻿using Amazon.S3;
+using Amazon.SimpleNotificationService;
+using Amazon.SQS;
+using Momo.Expectations;
+using Momo.Models;
+using Momo.Steps;
+using NSubstitute;
+
+namespace Momo.Tests.Expectations;
+
+public class MomoMongoExpectationTests
+{
+    private MomoClientFactoryOptions _options = new()
+    {
+        ExpectationFile = null,
+        s3Client = Substitute.For<IAmazonS3>(),
+        snsClient = Substitute.For<IAmazonSimpleNotificationService>(),
+        sqsClient = Substitute.For<IAmazonSQS>()
+    };
+    
+    [Test]
+    [TestCase(typeof(MongoStepHandler))]
+    public void GetStepHandler_Returns_TheCorrectStepHandler(Type expectedType)
+    {
+        //Arrange
+        var expectation = new MomoMongoExpectation()
+        {
+            ConnectionString = "mongodb://testuser:testpass@localhost:27017/testdb"
+        };
+
+        //Act
+        var stepHandler = expectation.GetStepHandler(_options);
+
+        //Assert
+        Assert.That(stepHandler, Is.TypeOf(expectedType));
+    }
+
+    [Test]
+    [TestCase("mongo://:@localhost:27017/")]
+    [TestCase("")]
+    [TestCase(null)]
+    public void GetStepHandler_Throws_WhenConnectionStringIsNotAMongoOnes(string? connectionString)
+    {
+        //Arrange
+        var expectation = new MomoMongoExpectation()
+        {
+            ConnectionString = connectionString!
+        };
+
+        //Act
+        Assert.Throws<InvalidOperationException>(() => expectation.GetStepHandler(_options),
+            "Not a mongo connection string");
+    }
+}
